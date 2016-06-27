28655428b026ae5f9f1511b7fd45caf49f670a8a
Navid <navid.rad@cern.ch>
2016-06-27 22:25:58 +0200

    CMG-DegStop part2

---
+++ RootTools/python/samples/triggers_13TeV_Spring15_degStop.py
@@ -0,0 +1,271 @@
+################## 
+## Triggers for HLT_MC_SPRING15 and Run II
+## Based on HLT_MC_SPRING15 and /frozen/2015/25ns14e33/v2.1/HLT/V1 and /frozen/2015/50ns_5e33/v2.1/HLT/V5
+
+
+### ----> for the degStop
+
+
+
+
+#  MC is NoiseCleaned but data will be JetIdCleaned
+
+#triggers_HLT_CaloMHTNoPU90_PFMET90_PFMHT90_IDTight_BTagCSV0p72                       =['HLT_CaloMHTNoPU90_PFMET90_PFMHT90_IDTight_BTagCSV0p72_v*']
+#triggers_HLT_CaloMHTNoPU90_PFMET90_PFMHT90_IDTight                                   =['HLT_CaloMHTNoPU90_PFMET90_PFMHT90_IDTight_v*']
+#triggers_HLT_MET200                                                                  =['HLT_MET200_JetIdCleaned_v*', 'HLT_MET200_v*']
+#triggers_HLT_MET250                                                                  =['HLT_MET250_v*']
+#triggers_HLT_MET300                                                                  =['HLT_MET300_v*']
+#triggers_HLT_MET60_IsoTrk35_Loose                                                    =['HLT_MET60_IsoTrk35_Loose_v*']
+#triggers_HLT_MET75_IsoTrk50                                                          =['HLT_MET75_IsoTrk50_v*']
+#triggers_HLT_MET90_IsoTrk50                                                          =['HLT_MET90_IsoTrk50_v*']
+#triggers_HLT_MonoCentralPFJet80_PFMETNoMu120_PFMH_PFMHTNoMu120_IDTight               =['HLT_MonoCentralPFJet80_PFMETNoMu120_JetIdCleaned_PFMHTNoMu120_IDTight_v*' , 'HLT_MonoCentralPFJet80_PFMETNoMu120_PFMHTNoMu120_IDTight_v*'] 
+#triggers_HLT_MonoCentralPFJet80_PFMETNoMu90_PFMHT_PFMHTNoMu90_IDTight                =['HLT_MonoCentralPFJet80_PFMETNoMu90_JetIdCleaned_PFMHTNoMu90_IDTight_v*', 'HLT_MonoCentralPFJet80_PFMETNoMu90_PFMHTNoMu90_IDTight_v*']
+#triggers_HLT_MonoCentralPFJet80_PFMETNoMu120_PFMH_PFMHTNoMu120_IDLoose               =['HLT_MonoCentralPFJet80_PFMETNoMu120_JetIdCleaned_PFMHTNoMu120_IDLoose_v*' , 'HLT_MonoCentralPFJet80_PFMETNoMu120_PFMHTNoMu120_IDLoose_v*'] 
+#triggers_HLT_MonoCentralPFJet80_PFMETNoMu90_PFMHT_PFMHTNoMu90_IDLoose                =['HLT_MonoCentralPFJet80_PFMETNoMu90_JetIdCleaned_PFMHTNoMu90_IDLoose_v*', 'HLT_MonoCentralPFJet80_PFMETNoMu90_PFMHTNoMu90_IDLoose_v*']
+#triggers_HLT_Mu14er_PFMET100                                                         =['HLT_Mu14er_PFMET100_JetIdCleaned_v*'                  , 'HLT_Mu14er_PFMET100_v*'                  ]
+#triggers_HLT_Mu3er_PFHT140_PFMET125                                                  =['HLT_Mu3er_PFHT140_PFMET125_JetIdCleaned_v*'           , 'HLT_Mu3er_PFHT140_PFMET125_v*'           ]
+#triggers_HLT_Mu6_PFHT200_PFMET100                                                    =['HLT_Mu6_PFHT200_PFMET100_JetIdCleaned_v*'             , 'HLT_Mu6_PFHT200_PFMET100_v*'             ]
+#triggers_HLT_Mu6_PFHT200_PFMET80_BTagCSV0p72                                         =['HLT_Mu6_PFHT200_PFMET80_JetIdCleaned_BTagCSV0p72_v*'  , 'HLT_Mu6_PFHT200_PFMET80_BTagCSV0p72_v*'  ]
+#triggers_HLT_PFMET100_PFMHT100_IDTight                                               =['HLT_PFMET100_PFMHT100_IDTight_v*']
+#triggers_HLT_PFMET100_PFMHT100_IDLoose                                               =['HLT_PFMET100_PFMHT100_IDLoose_v*']
+#triggers_HLT_PFMET110_PFMHT110_IDTight                                               =['HLT_PFMET110_PFMHT110_IDTight_v*']
+#triggers_HLT_PFMET110_PFMHT110_IDLoose                                               =['HLT_PFMET110_PFMHT110_IDLoose_v*']
+#triggers_HLT_PFMET120_BTagCSV0p72                                                    =['HLT_PFMET120_JetIdCleaned_BTagCSV0p72_v*', 'HLT_PFMET120_BTagCSV0p72_v*']
+#triggers_HLT_PFMET120_Mu5                                                            =['HLT_PFMET120_JetIdCleaned_Mu5_v*', 'HLT_PFMET120_Mu5_v*']
+#triggers_HLT_PFMET120_PFMHT120_IDTight                                               =['HLT_PFMET120_PFMHT120_IDTight_v*']
+#triggers_HLT_PFMET120_PFMHT120_IDLoose                                               =['HLT_PFMET120_PFMHT120_IDLoose_v*']
+#triggers_HLT_PFMET170_HBHECleaned                                                    =['HLT_PFMET170_HBHECleaned_v*']
+#triggers_HLT_PFMET170_JetIdCleaned                                                   =['HLT_PFMET170_JetIdCleaned_v*'  ]
+#triggers_HLT_PFMET170_NoiseCleaned                                                   =['HLT_PFMET170_NoiseCleaned_v*']
+#triggers_HLT_PFMET170                                                                =['HLT_PFMET170_v*']
+#triggers_HLT_PFMET300                                                                =['HLT_PFMET300_JetIdCleaned_v*'   ,'HLT_PFMET300_v*']
+#triggers_HLT_PFMET400                                                                =['HLT_PFMET400_JetIdCleaned_v*'   ,'HLT_PFMET400_v*']
+#triggers_HLT_PFMET90_PFMHT90_IDTight                                                 =['HLT_PFMET90_PFMHT90_IDTight_v*']
+#triggers_HLT_PFMET90_PFMHT90_IDLoose                                                 =['HLT_PFMET90_PFMHT90_IDLoose_v*']
+#triggers_HLT_PFMETNoMu120_PFMHTNoMu120_IDTight                                       =['HLT_PFMETNoMu120_JetIdCleaned_PFMHTNoMu120_IDTight_v*', 'HLT_PFMETNoMu120_PFMHTNoMu120_IDTight_v*'  ] 
+#triggers_HLT_PFMETNoMu90_PFMHTNoMu90_IDTight                                         =['HLT_PFMETNoMu90_JetIdCleaned_PFMHTNoMu90_IDTight_v*',    'HLT_PFMETNoMu90_PFMHTNoMu90_IDTight_v*'   ]
+#triggers_HLT_PFMETNoMu120_PFMHTNoMu120_IDLoose                                       =['HLT_PFMETNoMu120_JetIdCleaned_PFMHTNoMu120_IDLoose_v*', 'HLT_PFMETNoMu120_PFMHTNoMu120_IDLoose_v*'  ] 
+#triggers_HLT_PFMETNoMu90_PFMHTNoMu90_IDLoose                                         =['HLT_PFMETNoMu90_JetIdCleaned_PFMHTNoMu90_IDLoose_v*',    'HLT_PFMETNoMu90_PFMHTNoMu90_IDLoose_v*'   ]
+#
+#
+#
+#### SingleMuon PD:
+#triggers_HLT_IsoMu17_eta2p1   =['HLT_IsoMu17_eta2p1_v*'] 
+#triggers_HLT_IsoMu18          =['HLT_IsoMu18_v*']        
+#triggers_HLT_IsoMu20          =['HLT_IsoMu20_v*']        
+#triggers_HLT_IsoMu22          =['HLT_IsoMu22_v*']        
+#triggers_HLT_IsoMu27          =['HLT_IsoMu27_v*']        
+#triggers_HLT_IsoTkMu18        =['HLT_IsoTkMu18_v*']      
+#triggers_HLT_IsoTkMu20        =['HLT_IsoTkMu20_v*']      
+#triggers_HLT_IsoTkMu22        =['HLT_IsoTkMu22_v*']      
+#triggers_HLT_IsoTkMu27        =['HLT_IsoTkMu27_v*']      
+#triggers_HLT_L1SingleMu16     =['HLT_L1SingleMu16_v*']   
+#triggers_HLT_L1SingleMuOpen   =['HLT_L1SingleMuOpen_v*'] 
+#triggers_HLT_L2Mu10           =['HLT_L2Mu10_v*']         
+#triggers_HLT_Mu20             =['HLT_Mu20_v*']           
+#triggers_HLT_Mu24_eta2p1      =['HLT_Mu24_eta2p1_v*']    
+#triggers_HLT_Mu27             =['HLT_Mu27_v*']           
+#triggers_HLT_Mu45_eta2p1      =['HLT_Mu45_eta2p1_v*']    
+#triggers_HLT_Mu50             =['HLT_Mu50_v*']           
+#triggers_HLT_Mu55             =['HLT_Mu55_v*']           
+#triggers_HLT_TkMu20           =['HLT_TkMu20_v*']         
+#triggers_HLT_TkMu24_eta2p1    =['HLT_TkMu24_eta2p1_v*']  
+#triggers_HLT_TkMu27           =['HLT_TkMu27_v*']         
+#
+#
+#
+#
+#
+#### SingleElectron PD:
+#trigger_HLT_Ele300_CaloIdVT_GsfTrkIdT                                        =  ["HLT_Ele300_CaloIdVT_GsfTrkIdT_v*"]
+#trigger_HLT_Ele22_eta2p1_WPLoose_Gsf                                         =  ["HLT_Ele22_eta2p1_WPLoose_Gsf_v*"]
+#trigger_HLT_Ele27_WPLoose_Gsf_WHbbBoost                                      =  ["HLT_Ele27_WPLoose_Gsf_WHbbBoost_v*"]
+#trigger_HLT_Ele35_WPLoose_Gsf                                                =  ["HLT_Ele35_WPLoose_Gsf_v*"]
+#trigger_HLT_Ele50_IsoVVVL_PFHT400                                            =  ["HLT_Ele50_IsoVVVL_PFHT400_v*"]
+#trigger_HLT_Ele27_eta2p1_WPLoose_Gsf_DoubleMediumIsoPFTau40_Trk1_eta2p1_Reg  =  ["HLT_Ele27_eta2p1_WPLoose_Gsf_DoubleMediumIsoPFTau40_Trk1_eta2p1_Reg_v*"]
+#trigger_HLT_Ele23_WPLoose_Gsf_WHbbBoost                                      =  ["HLT_Ele23_WPLoose_Gsf_WHbbBoost_v*"]
+#trigger_HLT_Ele27_eta2p1_WPLoose_Gsf_LooseIsoPFTau20_SingleL1                =  ["HLT_Ele27_eta2p1_WPLoose_Gsf_LooseIsoPFTau20_SingleL1_v*"]
+#trigger_HLT_Ele27_eta2p1_WPLoose_Gsf_DoubleMediumIsoPFTau35_Trk1_eta2p1_Reg  =  ["HLT_Ele27_eta2p1_WPLoose_Gsf_DoubleMediumIsoPFTau35_Trk1_eta2p1_Reg_v*"]
+#trigger_HLT_Ele15_IsoVVVL_PFHT350_PFMET50                                    =  ["HLT_Ele15_IsoVVVL_PFHT350_PFMET50_v*"]
+#trigger_HLT_Ele15_IsoVVVL_PFHT350                                            =  ["HLT_Ele15_IsoVVVL_PFHT350_v*"]
+#trigger_HLT_Ele15_IsoVVVL_PFHT400_PFMET50                                    =  ["HLT_Ele15_IsoVVVL_PFHT400_PFMET50_v*"]
+#trigger_HLT_Ele115_CaloIdVT_GsfTrkIdT                                        =  ["HLT_Ele115_CaloIdVT_GsfTrkIdT_v*"]
+#trigger_HLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau20_SingleL1                =  ["HLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau20_SingleL1_v*"]
+#trigger_HLT_Ele27_WPLoose_Gsf                                                =  ["HLT_Ele27_WPLoose_Gsf_v*"]
+#trigger_HLT_Ele32_eta2p1_WPLoose_Gsf_LooseIsoPFTau20_SingleL1                =  ["HLT_Ele32_eta2p1_WPLoose_Gsf_LooseIsoPFTau20_SingleL1_v*"]
+#trigger_HLT_Ele15_IsoVVVL_BTagCSV_p067_PFHT400                               =  ["HLT_Ele15_IsoVVVL_BTagCSV_p067_PFHT400_v*"]
+#trigger_HLT_Ele250_CaloIdVT_GsfTrkIdT                                        =  ["HLT_Ele250_CaloIdVT_GsfTrkIdT_v*"]
+#trigger_HLT_Ele22_eta2p1_WPLoose_Gsf_LooseIsoPFTau20_SingleL1                =  ["HLT_Ele22_eta2p1_WPLoose_Gsf_LooseIsoPFTau20_SingleL1_v*"]
+#trigger_HLT_Ele27_eta2p1_WPLoose_Gsf                                         =  ["HLT_Ele27_eta2p1_WPLoose_Gsf_v*"]
+#trigger_HLT_Ele15_IsoVVVL_PFHT400                                            =  ["HLT_Ele15_IsoVVVL_PFHT400_v*"]
+#trigger_HLT_Ele23_WPLoose_Gsf                                                =  ["HLT_Ele23_WPLoose_Gsf_v*"]
+#trigger_HLT_Ele25_eta2p1_WPLoose_Gsf                                         =  ["HLT_Ele25_eta2p1_WPLoose_Gsf_v*"]
+#trigger_HLT_Ele35_CaloIdVT_GsfTrkIdT_PFJet150_PFJet50                        =  ["HLT_Ele35_CaloIdVT_GsfTrkIdT_PFJet150_PFJet50_v*"]
+#trigger_HLT_Ele45_WPLoose_Gsf                                                =  ["HLT_Ele45_WPLoose_Gsf_v*"]
+#trigger_HLT_Ele45_CaloIdVT_GsfTrkIdT_PFJet200_PFJet50                        =  ["HLT_Ele45_CaloIdVT_GsfTrkIdT_PFJet200_PFJet50_v*"]
+#trigger_HLT_Ele105_CaloIdVT_GsfTrkIdT                                        =  ["HLT_Ele105_CaloIdVT_GsfTrkIdT_v*"]
+#trigger_HLT_Ele27_eta2p1_WPLoose_Gsf_HT200                                   =  ["HLT_Ele27_eta2p1_WPLoose_Gsf_HT200_v*"]
+#trigger_HLT_Ele15_IsoVVVL_PFHT600                                            =  ["HLT_Ele15_IsoVVVL_PFHT600_v*"]
+#trigger_HLT_Ele24_eta2p1_WPLoose_Gsf                                         =  ["HLT_Ele24_eta2p1_WPLoose_Gsf_v*"]
+#trigger_HLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau20                         =  ["HLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau20_v*"]
+#trigger_HLT_Ele27_eta2p1_WPLoose_Gsf_DoubleMediumIsoPFTau32_Trk1_eta2p1_Reg  =  ["HLT_Ele27_eta2p1_WPLoose_Gsf_DoubleMediumIsoPFTau32_Trk1_eta2p1_Reg_v*"]
+#trigger_HLT_Ele45_WPLoose_Gsf_L1JetTauSeeded                                 =  ["HLT_Ele45_WPLoose_Gsf_L1JetTauSeeded_v*"]
+#trigger_HLT_Ele50_CaloIdVT_GsfTrkIdT_PFJet140                                =  ["HLT_Ele50_CaloIdVT_GsfTrkIdT_PFJet140_v*"]
+#trigger_HLT_Ele50_CaloIdVT_GsfTrkIdT_PFJet165                                =  ["HLT_Ele50_CaloIdVT_GsfTrkIdT_PFJet165_v*"]
+
+
+            
+           
+              
+    
+        
+METTriggers = \
+[\
+ 'HLT_CaloMHTNoPU90_PFMET90_PFMHT90_IDTight_BTagCSV_p067_v*',
+ 'HLT_CaloMHTNoPU90_PFMET90_PFMHT90_IDTight_v*',
+ 'HLT_DiCentralPFJet55_PFMET110_v*',
+ 'HLT_DoubleMu3_PFMET50_v*',
+ 'HLT_MET200_v*',
+ 'HLT_MET250_v*',
+ 'HLT_MET300_v*',
+ 'HLT_MET600_v*',
+ 'HLT_MET60_IsoTrk35_Loose_v*',
+ 'HLT_MET700_v*',
+ 'HLT_MET75_IsoTrk50_v*',
+ 'HLT_MET90_IsoTrk50_v*',
+ 'HLT_MonoCentralPFJet80_PFMETNoMu100_PFMHTNoMu100_IDTight_v*',
+ 'HLT_MonoCentralPFJet80_PFMETNoMu110_PFMHTNoMu110_IDTight_v*',
+ 'HLT_MonoCentralPFJet80_PFMETNoMu120_PFMHTNoMu120_IDTight_v*',
+ 'HLT_MonoCentralPFJet80_PFMETNoMu90_PFMHTNoMu90_IDTight_v*',
+ 'HLT_Mu14er_PFMET100_v*',
+ 'HLT_Mu3er_PFHT140_PFMET125_v*',
+ 'HLT_Mu6_PFHT200_PFMET100_v*',
+ 'HLT_Mu6_PFHT200_PFMET80_BTagCSV_p067_v*',
+ 'HLT_PFMET100_PFMHT100_IDTight_v*',
+ 'HLT_PFMET110_PFMHT110_IDTight_v*',
+ 'HLT_PFMET120_BTagCSV_p067_v*',
+ 'HLT_PFMET120_Mu5_v*',
+ 'HLT_PFMET120_PFMHT120_IDTight_v*',
+ 'HLT_PFMET170_BeamHaloCleaned_v*',
+ 'HLT_PFMET170_HBHECleaned_v*',
+ 'HLT_PFMET170_JetIdCleaned_v*',
+ 'HLT_PFMET170_NoiseCleaned_v*',
+ 'HLT_PFMET170_NotCleaned_v*',
+ 'HLT_PFMET300_v*',
+ 'HLT_PFMET400_v*',
+ 'HLT_PFMET500_v*',
+ 'HLT_PFMET600_v*',
+ 'HLT_PFMET90_PFMHT90_IDTight_v*',
+ 'HLT_PFMETNoMu100_PFMHTNoMu100_IDTight_v*',
+ 'HLT_PFMETNoMu110_PFMHTNoMu110_IDTight_v*',
+ 'HLT_PFMETNoMu120_PFMHTNoMu120_IDTight_v*',
+ 'HLT_PFMETNoMu90_PFMHTNoMu90_IDTight_v*'
+]
+
+
+SingleElPDTriggers = \
+[\
+ 'HLT_Ele105_CaloIdVT_GsfTrkIdT_v*',
+ 'HLT_Ele115_CaloIdVT_GsfTrkIdT_v*',
+ 'HLT_Ele15_IsoVVVL_BTagCSV_p067_PFHT400_v*',
+ 'HLT_Ele15_IsoVVVL_PFHT350_PFMET50_v*',
+ 'HLT_Ele15_IsoVVVL_PFHT350_v*',
+ 'HLT_Ele15_IsoVVVL_PFHT400_PFMET50_v*',
+ 'HLT_Ele15_IsoVVVL_PFHT400_v*',
+ 'HLT_Ele15_IsoVVVL_PFHT600_v*',
+ 'HLT_Ele22_eta2p1_WPLoose_Gsf_LooseIsoPFTau20_SingleL1_v*',
+ 'HLT_Ele22_eta2p1_WPLoose_Gsf_v*',
+ 'HLT_Ele23_WPLoose_Gsf_WHbbBoost_v*',
+ 'HLT_Ele23_WPLoose_Gsf_v*',
+ 'HLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau20_SingleL1_v*',
+ 'HLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau20_v*',
+ 'HLT_Ele24_eta2p1_WPLoose_Gsf_v*',
+ 'HLT_Ele250_CaloIdVT_GsfTrkIdT_v*',
+ 'HLT_Ele25_WPTight_Gsf_v*',
+ 'HLT_Ele25_eta2p1_WPLoose_Gsf_v*',
+ 'HLT_Ele25_eta2p1_WPTight_Gsf_v*',
+ 'HLT_Ele27_WPLoose_Gsf_WHbbBoost_v*',
+ 'HLT_Ele27_WPLoose_Gsf_v*',
+ 'HLT_Ele27_WPTight_Gsf_L1JetTauSeeded_v*',
+ 'HLT_Ele27_WPTight_Gsf_v*',
+ 'HLT_Ele27_eta2p1_WPLoose_Gsf_DoubleMediumIsoPFTau32_Trk1_eta2p1_Reg_v*',
+ 'HLT_Ele27_eta2p1_WPLoose_Gsf_DoubleMediumIsoPFTau35_Trk1_eta2p1_Reg_v*',
+ 'HLT_Ele27_eta2p1_WPLoose_Gsf_DoubleMediumIsoPFTau40_Trk1_eta2p1_Reg_v*',
+ 'HLT_Ele27_eta2p1_WPLoose_Gsf_HT200_v*',
+ 'HLT_Ele27_eta2p1_WPLoose_Gsf_LooseIsoPFTau20_SingleL1_v*',
+ 'HLT_Ele27_eta2p1_WPLoose_Gsf_v*',
+ 'HLT_Ele27_eta2p1_WPTight_Gsf_v*',
+ 'HLT_Ele300_CaloIdVT_GsfTrkIdT_v*',
+ 'HLT_Ele32_eta2p1_WPLoose_Gsf_LooseIsoPFTau20_SingleL1_v*',
+ 'HLT_Ele32_eta2p1_WPTight_Gsf_v*',
+ 'HLT_Ele35_CaloIdVT_GsfTrkIdT_PFJet150_PFJet50_v*',
+ 'HLT_Ele35_WPLoose_Gsf_v*',
+ 'HLT_Ele45_CaloIdVT_GsfTrkIdT_PFJet200_PFJet50_v*',
+ 'HLT_Ele45_WPLoose_Gsf_L1JetTauSeeded_v*',
+ 'HLT_Ele45_WPLoose_Gsf_v*',
+ 'HLT_Ele50_CaloIdVT_GsfTrkIdT_PFJet140_v*',
+ 'HLT_Ele50_CaloIdVT_GsfTrkIdT_PFJet165_v*',
+ 'HLT_Ele50_IsoVVVL_PFHT400_v*'
+]
+
+
+SingleMuonPDTriggers=\
+[\
+ 'HLT_DiMu9_Ele9_CaloIdL_TrackIdL_v*',
+ 'HLT_Mu12_Photon25_CaloIdL_L1ISO_v*',
+ 'HLT_Mu12_Photon25_CaloIdL_L1OR_v*',
+ 'HLT_Mu12_Photon25_CaloIdL_v*',
+ 'HLT_Mu17_Photon22_CaloIdL_L1ISO_v*',
+ 'HLT_Mu17_Photon30_CaloIdL_L1ISO_v*',
+ 'HLT_Mu17_Photon35_CaloIdL_L1ISO_v*',
+ 'HLT_Mu17_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v*',
+ 'HLT_Mu23NoFiltersNoVtx_Photon23_CaloIdL_v*',
+ 'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v*',
+ 'HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_v*',
+ 'HLT_Mu27_Ele37_CaloIdL_GsfTrkIdVL_v*',
+ 'HLT_Mu28NoFiltersNoVtxDisplaced_Photon28_CaloIdL_v*',
+ 'HLT_Mu30_Ele30_CaloIdL_GsfTrkIdVL_v*',
+ 'HLT_Mu33NoFiltersNoVtxDisplaced_Photon33_CaloIdL_v*',
+ 'HLT_Mu37_Ele27_CaloIdL_GsfTrkIdVL_v*',
+ 'HLT_Mu38NoFiltersNoVtx_Photon38_CaloIdL_v*',
+ 'HLT_Mu42NoFiltersNoVtx_Photon42_CaloIdL_v*',
+ 'HLT_Mu8_DiEle12_CaloIdL_TrackIdL_v*',
+ 'HLT_Mu8_Ele8_CaloIdM_TrackIdM_Mass8_PFHT250_v*',
+ 'HLT_Mu8_Ele8_CaloIdM_TrackIdM_Mass8_PFHT300_v*',
+ 'HLT_Mu8_TrkIsoVVL_Ele17_CaloIdL_TrackIdL_IsoVL_v*',
+ 'HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v*',
+]
+
+                                                  
+triggers = METTriggers + SingleMuonPDTriggers + SingleElPDTriggers
+
+for trigger in  triggers:
+  trigger_name = "trigger_{trig}".format(trig=trigger.replace("_v*","") )
+  exec("{trig_name}  =  [ '{trig}'] ".format(trig_name = trigger_name, trig=trigger))
+                                           
+                
+        
+                
+            
+                                    
+    
+                                    
+                     
+            
+                                    
+           
+        
+    
+           
+                            
+    
+                            
+            
+                 
+        
+           
+                           
+                                                  
+                   
+                    
+                    
