27273541baba09f22317301c8f79bb7440da5e71
schoefbeck <robert.schoefbeck@cern.ch>
2016-12-12 16:56:43 +0100

    samples

--- StopsDilepton/crab_with_das/heppy_samples.py
+++ StopsDilepton/crab_with_das/heppy_samples.py
@@ -4,7 +4,7 @@
 from CMGTools.RootTools.samples.samples_13TeV_RunIISpring16MiniAODv2 import *
 from CMGTools.RootTools.samples.samples_13TeV_DATA2016 import *
 from CMGTools.RootTools.samples.samples_13TeV_signals import *
-#from CMGTools.RootTools.samples.TTbarDMJets_signals_RunIISpring16MiniAODv2 import *
+from CMGTools.StopsDilepton.TTbarDMJets_signals_RunIISpring16MiniAODv2 import *
 #from CMGTools.StopsDilepton.samples_13TeV_Moriond2017 import *
 
 #from CMGTools.RootTools.samples.TTbarDMJets_signals_RunIISpring15MiniAODv2 import *
