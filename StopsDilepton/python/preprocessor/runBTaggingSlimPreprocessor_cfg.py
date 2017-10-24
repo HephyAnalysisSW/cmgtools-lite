5ccccbc15038cf5780b803973aed2e7c06c41633
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2017-10-24 10:16:07 +0200

    recipe

--- StopsDilepton/python/preprocessor/runBTaggingSlimPreprocessor_cfg.py
+++ StopsDilepton/python/preprocessor/runBTaggingSlimPreprocessor_cfg.py
@@ -68,11 +68,16 @@ updateJetCollection(
    jetSource = cms.InputTag('slimmedJets'),                                                                                                                                                
    jetCorrections = ('AK4PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute']), 'None'),
    btagDiscriminators = [
-    'pfDeepCSVJetTags:probudsg'        ,
-    'pfDeepCSVJetTags:probb'           ,
-    'pfDeepCSVJetTags:probc'           ,
-    'pfDeepCSVJetTags:probbb'          ,
-    'pfDeepCSVJetTags:probcc'          ,
+    'deepFlavourJetTags:probudsg'        ,
+    'deepFlavourJetTags:probb'           ,
+    'deepFlavourJetTags:probc'           ,
+    'deepFlavourJetTags:probbb'          ,
+    'deepFlavourJetTags:probcc'          ,
+    #'pfDeepCSVJetTags:probudsg'        ,
+    #'pfDeepCSVJetTags:probb'           ,
+    #'pfDeepCSVJetTags:probc'           ,
+    #'pfDeepCSVJetTags:probbb'          ,
+    #'pfDeepCSVJetTags:probcc'          ,
     ]
 )
 
