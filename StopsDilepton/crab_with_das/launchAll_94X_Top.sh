6e8dfd3c1fb81bebc3f60a2aebd2ae22c6927725
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2018-02-06 12:34:25 +0100

    MET triggers

--- StopsDilepton/crab_with_das/launchAll_94X_Top.sh
+++ StopsDilepton/crab_with_das/launchAll_94X_Top.sh
@@ -19,11 +19,17 @@
 #python launch.py --remoteDir="94X_1l_v1" --unitsPerJob=2 --production_label="94X_1l_v1" SingleMuon_Run2017E_17Nov2017
 #python launch.py --remoteDir="94X_1l_v1" --unitsPerJob=2 --production_label="94X_1l_v1" SingleMuon_Run2017F_17Nov2017
 #
-python launch.py --remoteDir="94X_1l_v4" --unitsPerJob=2 --production_label="94X_1l_v4" MET_Run2017B_17Nov2017
-python launch.py --remoteDir="94X_1l_v4" --unitsPerJob=2 --production_label="94X_1l_v4" MET_Run2017C_17Nov2017
-python launch.py --remoteDir="94X_1l_v4" --unitsPerJob=2 --production_label="94X_1l_v4" MET_Run2017D_17Nov2017
-python launch.py --remoteDir="94X_1l_v4" --unitsPerJob=2 --production_label="94X_1l_v4" MET_Run2017E_17Nov2017
-python launch.py --remoteDir="94X_1l_v4" --unitsPerJob=2 --production_label="94X_1l_v4" MET_Run2017F_17Nov2017
+python launch.py --remoteDir="94X_1l_v6" --unitsPerJob=2 --production_label="94X_1l_v6" HTMHT_Run2017B_17Nov2017
+python launch.py --remoteDir="94X_1l_v6" --unitsPerJob=2 --production_label="94X_1l_v6" HTMHT_Run2017C_17Nov2017
+python launch.py --remoteDir="94X_1l_v6" --unitsPerJob=2 --production_label="94X_1l_v6" HTMHT_Run2017D_17Nov2017
+python launch.py --remoteDir="94X_1l_v6" --unitsPerJob=2 --production_label="94X_1l_v6" HTMHT_Run2017E_17Nov2017
+python launch.py --remoteDir="94X_1l_v6" --unitsPerJob=2 --production_label="94X_1l_v6" HTMHT_Run2017F_17Nov2017
+
+python launch.py --remoteDir="94X_1l_v6" --unitsPerJob=2 --production_label="94X_1l_v6" MET_Run2017B_17Nov2017
+python launch.py --remoteDir="94X_1l_v6" --unitsPerJob=2 --production_label="94X_1l_v6" MET_Run2017C_17Nov2017
+python launch.py --remoteDir="94X_1l_v6" --unitsPerJob=2 --production_label="94X_1l_v6" MET_Run2017D_17Nov2017
+python launch.py --remoteDir="94X_1l_v6" --unitsPerJob=2 --production_label="94X_1l_v6" MET_Run2017E_17Nov2017
+python launch.py --remoteDir="94X_1l_v6" --unitsPerJob=2 --production_label="94X_1l_v6" MET_Run2017F_17Nov2017
 #
 #python launch.py --remoteDir="94X_1l_v1" --unitsPerJob=2 --production_label="94X_1l_v1" JetHT_Run2017B_17Nov2017
 #python launch.py --remoteDir="94X_1l_v1" --unitsPerJob=2 --production_label="94X_1l_v1" JetHT_Run2017C_17Nov2017
