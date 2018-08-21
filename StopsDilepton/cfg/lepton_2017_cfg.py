bc91478188bbd7f286d0d078f77dadc8605ccf51
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2018-08-21 21:46:27 +0200

    lepton_2017_cfg.py and recipe updates

--- StopsDilepton/cfg/lepton_2017_cfg.py
+++ StopsDilepton/cfg/lepton_2017_cfg.py
@@ -10,7 +10,7 @@ from CMGTools.TTHAnalysis.analyzers.susyCore_modules_cff import *
 from CMGTools.StopsDilepton.AutoFillVectorTreeProducer  import * 
 from CMGTools.TTHAnalysis.analyzers.ntupleTypes import *
 
-print "Year: 2016"
+print "Year: 2017"
 
 # general flags & settings 
 minLeptons                  = 1
@@ -52,8 +52,8 @@ lepAna.inclusive_muon_pt = 0.
 # rho area for both lepton flavors
 lepAna.ele_isoCorr = "rhoArea"
 lepAna.mu_isoCorr  = "rhoArea"
-lepAna.mu_effectiveAreas = 'Spring15_25ns_v1'
-lepAna.ele_effectiveAreas = 'Spring15_25ns_v1'
+lepAna.mu_effectiveAreas = 'Fall17'
+lepAna.ele_effectiveAreas = 'Fall17'
 
 # Loose selection 
 lepAna.loose_electron_relIso = 0.5
@@ -61,16 +61,6 @@ lepAna.loose_muon_relIso     = 0.5
 
 lepAna.match_inclusiveLeptons=True
 lepAna.pfCandAssocDR         = 0.5
-
-# Secondary vertex analyzer
-from CMGTools.TTHAnalysis.analyzers.ttHSVAnalyzer import ttHSVAnalyzer
-ttHSVAna = cfg.Analyzer(
-    ttHSVAnalyzer, name="ttHSVAnalyzer",
-    do_mc_match = True,
-)
-# SV
-susyCoreSequence.insert(susyCoreSequence.index(lepAna),
-                        ttHSVAna)
 
 if doElectronScaleCorrections:
     era = '25ns'
@@ -102,10 +92,10 @@ jetAna.smearJets = False #should be false in susycore, already
 jetAna.calculateSeparateCorrections = True #should be true if recalibrate, otherwise L1 inconsistent
 jetAna.calculateType1METCorrection = True
 
-# 2016/17 JEC
+# 2017 JEC
 jetAna.applyL2L3Residual = "Data"
-jetAna.dataGT = [ ( -1, "Summer16_07Aug2017BCD_V10_DATA"), (276811, "Summer16_07Aug2017EF_V10_DATA"), (278801, "Summer16_07Aug2017GH_V10_DATA") ]
-jetAna.mcGT   = "Summer16_23Sep2016V3_MC"
+jetAna.dataGT = [ ( -1, "Fall17_17Nov2017B_V6_DATA"), ( 299337, "Fall17_17Nov2017C_V6_DATA"), ( 302030, "Fall17_17Nov2017D_V6_DATA"), ( 303435, "Fall17_17Nov2017E_V6_DATA"), ( 304911, "Fall17_17Nov2017F_V6_DATA") ]
+jetAna.mcGT   = "Fall17_17Nov2017_V6_MC"
 
 #add pfCand variables
 pfParticleType = NTupleObjectType("pfParticle", baseObjectTypes = [ particleType ], variables = [
@@ -153,9 +143,8 @@ leptonTypeSusy.variables.append(  NTupleVariable("isElectron_float",lambda lepto
 leptonTypeSusy.variables.append(  NTupleVariable("isMuon_float",lambda lepton : 1. if abs(lepton.pdgId())==13 else 0., help="isMuon") )
 
 leptonTypeSusy.variables.append(  NTupleVariable("isGlobalMuon_float",   lambda x : x.physObj.isGlobalMuon() if abs(x.pdgId())==13 else 1., help="Muon is global"))
-# store TTV lepton MVA in 2016 version
-leptonTypeSusy.variables.append(  NTupleVariable("mvaTTV",lambda lepton : getattr(lepton, 'mvaValueTTV2016', -1), help="Lepton MVA (TTV 2016 version)") )
-
+# store TTV lepton MVA in 2017 version
+leptonTypeSusy.variables.append(  NTupleVariable("mvaTTV",lambda lepton : getattr(lepton, 'mvaValueTTV2017', -1), help="Lepton MVA (TTV 2017 version)") )
 
 # MET
 metAna.recalibrate = "type1"
@@ -168,28 +157,30 @@ chsMETAna = cfg.Analyzer(
     )
 susyCoreSequence.append( chsMETAna )
 
-# MET filters
-from CMGTools.TTHAnalysis.analyzers.badChargedHadronAnalyzerSummer2016 import badChargedHadronAnalyzerSummer2016
-badChargedHadronAna = cfg.Analyzer(
-    badChargedHadronAnalyzerSummer2016, name = 'badChargedHadronAnaSummer2016',
-    muons='slimmedMuons',
-    packedCandidates = 'packedPFCandidates',
-)
-from CMGTools.TTHAnalysis.analyzers.badMuonAnalyzerSummer2016 import badMuonAnalyzerSummer2016
-badMuonAna = cfg.Analyzer(
-    badMuonAnalyzerSummer2016, name = 'badMuonAnaSummer2016',
-    muons='slimmedMuons',
-    packedCandidates = 'packedPFCandidates',
-    minMuPt=100,
-    postFix='',
-)
-
+# iso tracks
+isoTrackAna.setOff = False
 
 from CMGTools.TTHAnalysis.analyzers.ttHLepEventAnalyzer import ttHLepEventAnalyzer
 ttHEventAna = cfg.Analyzer(
     ttHLepEventAnalyzer, name="ttHLepEventAnalyzer",
     minJets25 = 0,
     )
+
+# Fat jets
+susyCoreSequence.insert(susyCoreSequence.index(ttHCoreEventAna),
+                        ttHFatJetAna)
+# SV
+susyCoreSequence.insert(susyCoreSequence.index(lepAna),
+                        ttHSVAna)
+
+#ISR jet counting
+from CMGTools.TTHAnalysis.analyzers.nIsrAnalyzer import NIsrAnalyzer
+nISRAna = cfg.Analyzer(
+    NIsrAnalyzer, name="NIsrAnalyzer",
+    minJets25 = 0,
+    )
+susyCoreSequence.insert(susyCoreSequence.index(ttHCoreEventAna),
+                        nISRAna)
 
 if storePackedCandidates:
     from CMGTools.TTHAnalysis.analyzers.packedCandidateAnalyzer import packedCandidateAnalyzer
@@ -218,33 +209,28 @@ selectedComponents = [
         ]
 
 if getHeppyOption("loadSamples"):
-    from CMGTools.RootTools.samples.samples_13TeV_RunIISummer16MiniAODv2 import *
-    from CMGTools.RootTools.samples.samples_13TeV_DATA2016 import *
-    from CMGTools.RootTools.samples.samples_13TeV_signals import *
-    from CMGTools.StopsDilepton.TTbarDMJets_signals_RunIISummer16MiniAODv2 import *
-    from CMGTools.StopsDilepton.ttX0j_5f_MLM_signals_RunIISummer16MiniAODv2 import *
-    from CMGTools.StopsDilepton.samples import *
-
+    from CMGTools.RootTools.samples.samples_13TeV_DATA2017 import *
+    from CMGTools.RootTools.samples.samples_13TeV_RunIIFall17MiniAOD import *
     for sample in dataSamples:
-        sample.json="$CMSSW_BASE/src/CMGTools/TTHAnalysis/data/json/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt"
+        sample.json="$CMSSW_BASE/src/CMGTools/TTHAnalysis/data/json/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt"
 
-    # sync mc
-    #selectedComponents = [TTJets_SingleLeptonFromTbar]
-    #TTJets_SingleLeptonFromTbar.files=['root://cms-xrd-global.cern.ch//store/mc/RunIISummer16MiniAODv2/TTJets_SingleLeptFromTbar_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/60000/00A25ADE-DFD4-E611-8EAC-0025905A48B2.root']
-    # sync data
-    #MuonEG_Run2016F_07Aug17.files=["root://cms-xrd-global.cern.ch//store/data/Run2016F/MuonEG/MINIAOD/07Aug17-v1/110000/0ADB9E32-B19A-E711-BCE0-008CFAC91A68.root"]
-    #selectedComponents = [MuonEG_Run2016F_07Aug17]
-    # test new MC
-    GluGluToZZTo2e2mu.files = ['root://cms-xrd-global.cern.ch//store/mc/RunIISummer16MiniAODv2/GluGluToContinToZZTo2e2mu_13TeV_MCFM701_pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/A669DB9C-B8BD-E611-B462-001E67E33C60.root']
-    selectedComponents = [GluGluToZZTo2e2mu]
+    #selectedComponents = [MuonEG_Run2017F_17Nov2017]
+    #MuonEG_Run2017F_17Nov2017.files = ["/afs/hephy.at/user/r/rschoefbeck/www/etc/pickevents.root"]
+    #MuonEG_Run2017F_17Nov2017.files = ['root://cms-xrd-global.cern.ch//store/data/Run2018B/DoubleMuon/MINIAOD/PromptReco-v1/000/317/383/00000/F8DC8A54-1669-E811-8656-02163E01A01E.root']
+    # sync
+    #TTSemi_pow.files = ['root://cms-xrd-global.cern.ch//store/mc/RunIIFall17MiniAOD/TTToSemiLeptonic_TuneCP5_PSweights_13TeV-powheg-pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v1/50000/004C666D-C0E0-E711-AADB-0CC47A6C183A.root']
+    #selectedComponents = [WWZ_4F]
+    #MuonEG_Run2017F_17Nov2017.files=['root://cms-xrd-global.cern.ch//store/data/Run2017F/MuonEG/MINIAOD/17Nov2017-v1/50000/02F38D9F-54EA-E711-A28D-02163E014331.root']
+    #selectedComponents += [MuonEG_Run2017F_17Nov2017]
+    selectedComponents = [TTZToLLNuNu_amc]
+    TTZToLLNuNu_amc.files = ['root://cms-xrd-global.cern.ch//store/mc/RunIIFall17MiniAOD/TTZToLLNuNu_M-10_TuneCP5_13TeV-amcatnlo-pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v1/20000/02A1FA28-F1F9-E711-AFB2-002590E3A0FC.root']
+    #selectedComponents = [ TTLep_pow ]
+    #TTLep_pow.files = ['root://cms-xrd-global.cern.ch//store/mc/RunIIFall17MiniAOD/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v2/60000/0857E231-22EF-E711-BEAC-FA163E2DB7A6.root']
+    #selectedComponents = [ GluGluToZZTo4mu ]
 
-    #selectedComponents = [ttZ0j_ll]
-    #selectedComponents = [WZTo3LNu]
-    #selectedComponents = [JetHT_Run2016B_03Feb2017_v2]
-    for comp in selectedComponents:
-        comp.files = comp.files[:1]
-        print comp.files
-        comp.splitFactor = 10
+for comp in selectedComponents:
+    comp.files = comp.files[:1]
+    comp.splitFactor = 1
 
 from CMGTools.TTHAnalysis.tools.EOSEventsWithDownload import EOSEventsWithDownload
 from PhysicsTools.HeppyCore.framework.eventsfwlite import Events
@@ -252,10 +238,7 @@ event_class = Events
 if getHeppyOption("fetch"):
   event_class = EOSEventsWithDownload
 
-preprocessorFile = "$CMSSW_BASE/python/CMGTools/StopsDilepton/preprocessor/runBTaggingSlimPreprocessor_cfg.py"
-from PhysicsTools.Heppy.utils.cmsswPreprocessor import CmsswPreprocessor
-preprocessor = CmsswPreprocessor(preprocessorFile)
-jetAna.jetCol = 'selectedUpdatedPatJets'
+preprocessor = None
 
 config = cfg.Config( components = selectedComponents,
                      sequence = sequence,
